5c400a19ad09ea5d7bb6e070b4345c7b01f832e3
tuancuongth88 <tuancuongth88@gmail.com>
2015-12-27 16:06:55 +0700

    cuongnt add model db

---
+++ projectDatSan/projectDatSan/Models/message.cs
@@ -0,0 +1,20 @@
+namespace projectDatSan.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("message")]
+    public partial class message
+    {
+        public int id { get; set; }
+
+        [StringLength(50)]
+        public string email { get; set; }
+
+        [StringLength(500)]
+        public string descript { get; set; }
+    }
+}
